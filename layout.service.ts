5d63396c30ac47f2b9050e84ef6fa8f00c86e5ee
alekseyti <68515628+alekseyti@users.noreply.github.com>
2023-05-06 12:22:27 +0300

    Create layout.service.ts

---
+++ layout.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+export const CustomBreakpointNames = {
+  extraSmall: 'extraSmall',
+  extraLarge: 'extraLarge'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutService {
+
+  activeBreakpoints: string[] = [];
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  breakpoints: object = {
+    '(max-width: 220px)': CustomBreakpointNames.extraSmall,
+    '(min-width: 2400px)': CustomBreakpointNames.extraLarge
+  }
+
+  subscribeToLayoutChanges() {
+    return this.breakpointObserver
+    .observe(Object.keys(this.breakpoints))
+    .pipe(map((observeResponse) => this.parseBreakpointsResponse(observeResponse.breakpoints)))
+  }
+
+  parseBreakpointsResponse(breakpoints): string[] {
+    this.activeBreakpoints = [];
+
+    Object.keys(breakpoints).map((key) => {
+      if (breakpoints[key]) {
+        this.activeBreakpoints.push(this.breakpoints[key])
+      }
+    })
+
+    return this.activeBreakpoints;
+  }
+
+  isBreakpointActive(breakpointName) {
+    return this.activeBreakpoints.find(breakpoint => breakpoint === breakpointName)
+  }
+  
+
+
+}
